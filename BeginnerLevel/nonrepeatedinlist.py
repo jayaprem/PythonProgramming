532edd07dd04835cf0335f9a0ece85f8d73a422b
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-06-01 19:52:56 -0700

    Create nonrepeatedinlist.py

---
+++ BeginnerLevel/nonrepeatedinlist.py
@@ -0,0 +1,22 @@
+n=int(input())
+list1=[]
+list2=[]
+count=0
+for i in range(0,n):
+    r=int(input())
+    list1.append(r)
+for j in range(0,n):
+    c=list1.count(list1[j])
+    if(c>1):
+        count=count+1
+    else:
+        list2.append(list1[j])
+if(count==n):
+    print ("repeated")
+else:
+    sorted(list2)
+    for x in range(len(list2)):
+        print(list2[x],end=' ')
+        
+    
+          
