efd31b6434414dc358bbaef4bf693cd75638a1ca
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-06-12 09:08:59 +0000

    Update removevowels.py

--- BeginnerLevel/removevowels.py
+++ BeginnerLevel/removevowels.py
@@ -3,8 +3,9 @@ list1=['a','e','i','o','u']
 list2=[]
 for i in range(0,n):
 	p=raw_input()
-	if p not in list1:
-		list2.append(p)
+	if(p.isalpha()):
+		if p not in list1:
+			list2.append(p)
 sorted(list2)
 list2.reverse()
 print ("".join(list2))		
