4bfb2b31376115a100d3f2bb9b0fd415f14ff865
GokulnthK <gokul1997in12@gmail.com>
2019-07-11 16:21:24 +0530

    search change

--- project-Goks/search.py
+++ project-Goks/search.py
@@ -1,17 +1,30 @@
 import speech_recognition as sr
 from googlesearch import search
+import os
+import webbrowser as web
 import sys
-r = sr.Recognizer()
-filename = sr.AudioFile('Modi.wav')
-with filename as source:
-    audio = r.listen(source)
-try:
+
+
+def speechToText():
+    r = sr.Recognizer()
+    filename = sr.AudioFile('Modi.wav')
+    with filename as source:
+        audio = r.listen(source)
     s = r.recognize_google(audio)
+    return s
+
+
+s = speechToText()
+try:
     arr = s.split()
     if arr[0] == 'search':
         arr.pop(0)
-        for j in search(arr[1], tld="co.in", stop=10, pause=2):
-            print(j)
+        for j in search(" ".join(arr), tld="co.in", stop=2, pause=2):
+            web.open_new_tab(j)
+            audio = speechToText()
+            s = speechToText()
+            if s == 'close':
+                os.system("taskkill /im chrome.exe /f")
 
 except Exception:
     print("something went wrong")
