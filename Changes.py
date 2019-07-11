6beef167a2daa33c71ea99afa9088fe05fba06df
gokulNth <42962398+gokulNth@users.noreply.github.com>
2019-07-11 12:04:07 +0530

    Create Changes.py

---
+++ Changes.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+from googlesearch import search
+import sys
+r=sr.Recognizer()
+filename=sr.AudioFile('Modi.wav')
+with filename as source:
+    audio=r.listen(source)
+try:
+    s=r.recognize_google(audio)
+    arr=s.split()
+    if arr[0]=='search':
+        arr.pop(0)
+        for j in search("".join(arr), tld="co.in",stop=10,pause=2):
+            print(j)
+
+except Exception:
+    print("something went wrong")
