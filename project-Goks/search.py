088ae4fd24ae5dbb4df83f24e6759fab60ec8c4c
GokulnthK <gokul1997in12@gmail.com>
2019-07-11 16:13:02 +0530

    changes

--- project-Goks/search.py
+++ project-Goks/search.py
@@ -1,17 +1,33 @@
 import speech_recognition as sr
 from googlesearch import search
-import sys
-r=sr.Recognizer()
-filename=sr.AudioFile('Modi.wav')
-with filename as source:
-    audio=r.listen(source)
+import os
+import webbrowser
+
+
+def speechToText():
+    filename = sr.AudioFile('Modi.wav')
+    with filename as source:
+        audio = r.listen(source)
+    return audio
+
+
+r = sr.Recognizer()
+audio = speechToText()
+
 try:
-    s=r.recognize_google(audio)
-    arr=s.split()
-    if arr[0]=='search':
+    a = []
+    s = r.recognize_google(audio)
+    arr = s.split()
+    if arr[0] == 'search':
         arr.pop(0)
-        for j in search("".join(arr), tld="co.in",stop=10,pause=2):
-            print(j)
+        for j in search(" ".join(arr), tld="co.in", stop=2, pause=2):
+            a.append(j)
+        webbrowser.open_new_tab(a[0])
+        s = speechToText()
+        if s == 'next':
+            webbrowser.open_new_tab(a[1])
+        if s == 'close':
+            os.system("taskkill /im chrome.exe /f")
 
 except Exception:
     print("something went wrong")
